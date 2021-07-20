7a101fe4119d11c3df4bfbf5db4549f1025b1e3d
Jexperto <ainebuzzed@gmail.com>
2021-07-20 16:48:33 +0300

    Added optional notification interval for users Added debug mode Fixed a bunch of bugs

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,6 @@
 import hashlib
 import io
+import os
 import shelve
 import sys
 import telebot
@@ -7,22 +8,23 @@ from threading import Timer
 from applicants_data import ApplicantsData
 from network import get_file
 
-
-table_path = '../resources/table.xls'
-token = open("../resources/token.txt", "r").read().replace("\n", "").replace("\r", "")
-subs_file = shelve.open("../resources/subscribers")
+wd = os.path.dirname(sys.modules['__main__'].__file__) + "/"
+with open(wd + "../resources/secret.txt", "a+"):
+    pass
+with open(wd + "../resources/token.txt", "a+"):
+    pass
+table_path = wd + '../resources/table.xls'
+token = open(wd + "../resources/token.txt", "r").read().replace("\n", "").replace("\r", "")
+subs_file = shelve.open(wd + "../resources/subscribers")
+admins_file = shelve.open(wd + "../resources/admins")
 subscribers = list(subs_file.keys())
-temp = open("../resources/secret.txt","a+")
-temp.close()
-temp = open("../resources/token.txt","a+")
-temp.close()
+
 args = []
 for i, arg in enumerate(sys.argv):
-    if i>0:
+    if i > 0:
         args.append(int(arg))
 interval = args[0] if len(args) > 0 else 3600
-download_timer = args[1] if len(args) > 0 else (interval if interval - 1 < 60 else 60)
-
+download_timer = args[1] if len(args) > 1 else 60
 
 commands = [['help', 'start'], ['get'], ['subscribe', 'sub'], ['unsubscribe', 'unsub'], ["amount"],
             ['point_summary', 'psum', 'opossum'], ['amount_applicants_higher_than', 'higher']]
@@ -39,7 +41,7 @@ def add_subscriber(sub_id):
     global subs_file
     if str(sub_id) in subscribers:
         return -1
-    subs_file[str(sub_id)] = False
+    subs_file[str(sub_id)] = {"int": -1, "current_int": 0, "notified": False}  # interval
     subscribers.append(str(sub_id))
     return 0
 
@@ -47,9 +49,11 @@ def add_subscriber(sub_id):
 def remove_subscriber(sub_id):
     global subscribers
     global subs_file
-    del subs_file[str(sub_id)]
-    subscribers.remove(str(sub_id))
-    return
+    if str(sub_id) in subs_file:
+        del subs_file[str(sub_id)]
+        subscribers.remove(str(sub_id))
+        return True
+    return False
 
 
 def reset_download():
@@ -60,46 +64,67 @@ def reset_download():
 def download_file():
     global can_download
     global file_handle
-    global raw_file
     if can_download:
-        raw_file = get_file()
+        file = get_file()
         can_download = False
-        file_handle = open(table_path, 'wb').write(raw_file)
+        file_handle = open(table_path, 'wb').write(file)
         t = Timer(interval=download_timer, function=reset_download)
         t.start()
+        return file
+    return False
 
 
-def send_update():
+def set_nested_object(top_level_obj, top_level_key, nested_key, value):
+    nested = top_level_obj[top_level_key]
+    nested[nested_key] = value
+    top_level_obj[top_level_key] = nested
+
+
+def start_timed_downloads():
     global last_file_hash
-
-    download_file()
-    md5 = hashlib.md5(raw_file).hexdigest()
-    updated = False if md5 == last_file_hash else True
-
-    if raw_file is None:
-        for key in subscribers:
+    global raw_file
+    file = download_file()
+    if not file:
+        return
+    if file is None:
+        for key in admins_file.keys():
             bot.send_message(key, "Something went wrong with getting the file")
+    md5 = hashlib.md5(file).hexdigest()
+    raw_file = file
+    file_updated = False if md5 == last_file_hash else True
+    if file_updated:
+        for key in subscribers:
+            set_nested_object(subs_file, key, "notified", False)
+    last_file_hash = md5
+    d_timer = Timer(interval=download_timer, function=start_timed_downloads)
+    d_timer.start()
+
+
+def notify_user(user_id, doc):
+    print(subs_file[user_id]["notified"])
+    if not subs_file[user_id]["notified"]:
+        set_nested_object(subs_file, user_id, "notified", True)
+        bot.send_message(user_id, "Полундра, файл проапдейтился")
+        bot.send_document(user_id, doc)
+        set_nested_object(subs_file, user_id, "current_int", 0)
+        return
+    current_int = int(subs_file[user_id]["current_int"])
+    user_total_interval = int(subs_file[user_id]["int"])
+    if user_total_interval < 0:
+        return
+    if current_int + interval > user_total_interval:
+        set_nested_object(subs_file, user_id, "current_int", 0)
+        bot.send_message(user_id, "Пока ничего не менялось")
     else:
+        set_nested_object(subs_file, user_id, "current_int", current_int + interval)
 
-        if updated:
-            print("current md5: ", md5, "last md5:", last_file_hash)
-            last_file_hash = md5
-            for key in subscribers:
-                doc = io.BytesIO(raw_file)
-                doc.name = "table.xls"
-                bot.send_document(key, doc)
-                subs_file[key] = True
-        else:
-            for key in subscribers:
-                if subs_file[key]:
-                    bot.send_message(key, "Пока ничего не менялось")
-                else:
-                    doc = io.BytesIO(raw_file)
-                    doc.name = "table.xls"
-                    bot.send_document(key, doc)
-                    subs_file[key] = True
 
-    t = Timer(interval=interval, function=send_update)
+def send_updates():
+    doc = io.BytesIO(raw_file)
+    doc.name = "table.xls"
+    for key in subscribers:
+        notify_user(key, doc)
+    t = Timer(interval=interval, function=send_updates)
     t.start()
 
 
@@ -116,15 +141,18 @@ def get_commands(message):
 def send_table(message):
     global raw_file
     global can_download
-
-    download_file()
-    if raw_file is None:
+    file = download_file()
+    user_id = str(message.chat.id)
+    if file is None:
         bot.send_message(message.chat.id, "Что-то пошло не так... Файла нет. \U0001F633")
     else:
-        if message.chat.id in subscribers:
-            subs_file[message.chat.id] = True
-            if subs_file[message.chat.id]:
+        if file:
+            raw_file = file
+        if user_id in subscribers:
+            if subs_file[user_id]["notified"]:
                 bot.send_message(message.chat.id, "Ничего не поменялось, но забирай \U0001F605")
+            else:
+                bot.send_message(message.chat.id, "Лови")
         doc = io.BytesIO(raw_file)
         doc.name = "table.xls"
         bot.send_document(message.chat.id, doc)
@@ -141,8 +169,10 @@ def sub(message):
 
 @bot.message_handler(commands=['unsubscribe', 'unsub'])
 def unsub(message):
-    remove_subscriber(message.chat.id)
-    bot.send_message(message.chat.id, 'Ты меня бросаешь? \U0001F62D')
+    if remove_subscriber(message.chat.id):
+        bot.send_message(message.chat.id, 'Ты меня бросаешь? \U0001F62D')
+    else:
+        bot.send_message(message.chat.id, 'Ты и так на меня не подписн')
 
 
 def parse_command(command):
@@ -221,16 +251,57 @@ def amount_applicants_higher_than(message):
     bot.send_message(message.chat.id, str(rs))
 
 
+@bot.message_handler(commands=['interval'])
+def set_personal_interval(message):
+    args = parse_command(message.text)
+    user_id = str(message.chat.id)
+    if len(args) > 0:
+        try:
+            i = int(args[0])
+            if i <= 0:
+                set_nested_object(subs_file, user_id, "int", -1)
+                bot.send_message(message.chat.id, "Окей, не буду беспокоить")
+                return
+            set_nested_object(subs_file, user_id, "int", i * 60)
+            last_digit = int(repr(i)[-1])
+            number = str(i)
+            if i == 1:
+                bot.send_message(message.chat.id,
+                                 "Буду писать тебе каждую минуту")
+            else:
+                bot.send_message(message.chat.id,
+                                 "Буду писать тебе " + "каждую " + number + " минуту" if last_digit == 1 else (
+                                     "каждые " + number + " минуты" if (
+                                             1 < last_digit < 4) else "каждые " + number + " минут"))
+        except ValueError:
+            bot.send_message(message.chat.id, "Число, Карл, мне нужно число...")
+
+
+@bot.message_handler(commands=['debug'])
+def add_admin(message):
+    global admins_file
+    user_id = str(message.chat.id)
+    args = parse_command(message.text)
+    if len(args) > 0:
+        if str(args[0]).lower() == "true" or str(args[0]).lower() == "1":
+            admins_file[user_id] = True
+            bot.send_message(message.chat.id, "Debug enabled")
+
+        elif str(args[0]).lower() == "false" or str(args[0]).lower() == "0":
+            if str(message.chat.id) in admins_file:
+                del admins_file[user_id]
+            bot.send_message(message.chat.id, "Debug disabled")
+
+
 @bot.message_handler(commands=['test'])
 def test(message):
     bot.send_message(message.chat.id, b"\xf0\x9f\x8d\x95")
-    print(message)
 
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
     args = parse_command(message.text)
-    secret = open('../resources/secret.txt',"r").readline().replace("\n", "")
+    secret = open(wd + '../resources/secret.txt', "r").readline().replace("\n", "")
     print(secret)
     if len(args) > 0 and args[0] == secret:
         bot.stop_polling()
@@ -239,6 +310,15 @@ def stop(message):
         bot.send_message(message.chat.id, "Ага, бегу и падаю \U0001F64A")
 
 
-timer = Timer(interval=interval, function=send_update)
+file = download_file()
+if file:
+    raw_file = file
+else:
+    with open(wd + "../resources/table.xls", "r+") as f:
+        raw_file = f.read()
+
+timer = Timer(interval=download_timer, function=start_timed_downloads)
+user_interval_timer = Timer(interval=interval, function=send_updates)
+user_interval_timer.start()
 timer.start()
 bot.polling()
